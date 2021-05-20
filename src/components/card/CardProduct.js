3d72538ed75483211e8a40c2d467b9099b51d11d
Baxi19 <randald1991@gmail.com>
2021-05-19 20:13:44 -0600

    Hover  and scale working :fire:

--- src/components/card/CardProduct.js
+++ src/components/card/CardProduct.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
   root: {
@@ -16,26 +17,40 @@ const useStyles = makeStyles({
   },
 
   img: {
-    width: "100%",
     borderRadius: "10%",
   },
 });
 
 export default function CardProduct(props) {
   const classes = useStyles();
+    const [hovered, setHover] = useState(false);
+
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia 
-          component="img"
-          alt={`IMAGE: ${props.item.name}`}
-          height="180"
-          image={`${props.item.image}`}
-          title={props.item.name}
-          className={classes.img}
-        />
         
+      <Box
+        borderRadius={16}
+        boxShadow={ hovered 
+            ? 3 
+            : 0
+        }
+        p={0.5}
+      >
+        <CardMedia 
+            component="img"
+            alt={`IMAGE: ${props.item.name}`}
+            height="180"
+            image={`${props.item.image}`}
+            title={props.item.name}
+            className={classes.img}
+            onMouseOut={() => setHover(false)}
+            onMouseOver={() => setHover(true)}
+            style={{transform: `${hovered ? 'scale(1.1,1.1)' : 'scale(1,1)'}`}}
+        />
+      </Box>
+
       </CardActionArea>
       <CardContent>
           <Typography gutterBottom variant="h5" component="h3">
